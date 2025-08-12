3823b47d79a1c627d903b2f722446c71a4a71014
RAJABABU KUMAR <rksinghbth500@gmail.com>
2025-08-13 00:39:53 +0530

    update Aggregation folder

--- sql/Aggregation/weather-observation-station-19.sql
+++ sql/Aggregation/weather-observation-station-19.sql
@@ -1,1 +1,8 @@
--- Solution for weather-observation-station-19.sql
+SELECT
+    ROUND(
+        SQRT(
+            POW(MAX(LAT_N) - MIN(LAT_N), 2) + POW(MAX(LONG_W) - MIN(LONG_W), 2)
+        ),
+        4
+    )
+FROM STATION;
