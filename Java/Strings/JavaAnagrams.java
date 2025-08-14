29301df5b7ded79678561effebcc6428c6205c0d
RAJABABU KUMAR <161234267+rksingh1713@users.noreply.github.com>
2025-08-14 17:27:22 +0530

    Update JavaAnagrams.java

--- Java/Strings/JavaAnagrams.java
+++ Java/Strings/JavaAnagrams.java
@@ -1,28 +1,47 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class JavaAnagrams {
-
-    static boolean isAnagram(String a, String b) {
-        if (a.equals(b)) {
-            return true;
-        } else {
-            char[] arr1 = a.toCharArray();
-            char[] arr2 = b.toCharArray();
-            Arrays.sort(arr1);
-            Arrays.sort(arr2);
-            return Arrays.equals(arr1, arr2);
+public class Solution {
+     static boolean isAnagram(String a, String b) {
+        // If lengths differ, they can't be anagrams
+        if (a.length() != b.length()) {
+            return false;
         }
 
+        // Convert both strings to lowercase
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        // Frequency array for English alphabets (26 letters)
+        int[] freq = new int[26];
+
+        // Count characters from 'a'
+        for (int i = 0; i < a.length(); i++) {
+            freq[a.charAt(i) - 'a']++;
+        }
+
+        // Subtract counts using 'b'
+        for (int i = 0; i < b.length(); i++) {
+            freq[b.charAt(i) - 'a']--;
+        }
+
+        // If all counts are zero, they are anagrams
+        for (int count : freq) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+    
         Scanner scan = new Scanner(System.in);
-        String a = scan.next().toLowerCase();
-        String b = scan.next().toLowerCase();
+        String a = scan.next();
+        String b = scan.next();
         scan.close();
         boolean ret = isAnagram(a, b);
-        System.out.println((ret) ? "Anagrams" : "Not Anagrams");
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
     }
 }
