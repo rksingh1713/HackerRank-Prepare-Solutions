49a1b230335af3d3d243e543a24b16ab8bb05a57
RAJABABU KUMAR <161234267+rksingh1713@users.noreply.github.com>
2025-08-14 16:40:34 +0530

    Update Java2DArray.java

--- Java/DataStructures/Java2DArray.java
+++ Java/DataStructures/Java2DArray.java
@@ -1,54 +1,33 @@
-import static java.util.stream.Collectors.toList;
+import java.util.Scanner;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+public class Solution {
+	static final int SIZE = 6;
+	static final int[] OFFSET_R = { -1, -1, -1, 0, 1, 1, 1 };
+	static final int[] OFFSET_C = { -1, 0, 1, 0, -1, 0, 1 };
 
-public class Java2DArray {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int[][] board = new int[SIZE][SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				board[i][j] = in.nextInt();
+			}
+		}
+		int maxSum = Integer.MIN_VALUE;
+		for (int i = 1; i < SIZE - 1; i++) {
+			for (int j = 1; j < SIZE - 1; j++) {
+				maxSum = Math.max(maxSum, computeHourglassesSum(board, i, j));
+			}
+		}
+		System.out.println(maxSum);
+		in.close();
+	}
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader bufferedReader = new BufferedReader(
-      new InputStreamReader(System.in)
-    );
-
-    List<List<Integer>> arr = new ArrayList<>();
-
-    IntStream
-      .range(0, 6)
-      .forEach(i -> {
-        try {
-          arr.add(
-            Stream
-              .of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-              .map(Integer::parseInt)
-              .collect(toList())
-          );
-        } catch (IOException ex) {
-          throw new RuntimeException(ex);
-        }
-      });
-
-    bufferedReader.close();
-
-    int maxSum = Integer.MIN_VALUE;
-    for (int i = 0; i < arr.size() - 2; i++) {
-      for (int j = 0; j < arr.get(i).size() - 2; j++) {
-        int glassSum =
-          arr.get(i).get(j) +
-          arr.get(i).get(j + 1) +
-          arr.get(i).get(j + 2) +
-          arr.get(i + 1).get(j + 1) +
-          arr.get(i + 2).get(j) +
-          arr.get(i + 2).get(j + 1) +
-          arr.get(i + 2).get(j + 2);
-        maxSum = Math.max(maxSum, glassSum);
-      }
-    }
-
-    System.out.println(maxSum);
-  }
+	static int computeHourglassesSum(int[][] board, int r, int c) {
+		int sum = 0;
+		for (int i = 0; i < OFFSET_R.length; i++) {
+			sum += board[r + OFFSET_R[i]][c + OFFSET_C[i]];
+		}
+		return sum;
+	}
 }
