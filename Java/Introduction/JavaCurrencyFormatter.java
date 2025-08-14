11a0add358ea4b0255f22666c3517697f8f7a87f
RAJABABU KUMAR <161234267+rksingh1713@users.noreply.github.com>
2025-08-14 16:37:27 +0530

    Update JavaCurrencyFormatter.java

--- Java/Introduction/JavaCurrencyFormatter.java
+++ Java/Introduction/JavaCurrencyFormatter.java
@@ -1,18 +1,29 @@
-/* Doesn´t work in Java 15  */
-
+import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.Scanner;
-import java.text.NumberFormat;
 
-public class JavaCurrencyFormatter {
+public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Double payment = sc.nextDouble();
+        double payment = sc.nextDouble();
         sc.close();
-        System.out.println("US: " + NumberFormat.getCurrencyInstance(Locale.US).format(payment));
-        NumberFormat fmt = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
-        System.out.println("India: " + fmt.format(payment));
-        System.out.println("China: " + NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment));
-        System.out.println("France: " + NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment));
+
+        // Format for US
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        
+        // Format for India (Java doesn't have predefined Locale for India)
+        String india = NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment);
+        
+        // Format for China
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        
+        // Format for France
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        // Print in the expected format
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
     }
 }
